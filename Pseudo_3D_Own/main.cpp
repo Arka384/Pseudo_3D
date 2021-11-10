7aeb65357952a35e9db6aa6386eea91495c6f932
Arka384 <67411746+Arka384@users.noreply.github.com>
2021-11-10 21:35:55 +0530

    Curvature tracking and movement

--- Pseudo_3D_Own/main.cpp
+++ Pseudo_3D_Own/main.cpp
@@ -13,22 +13,33 @@ std::vector<sf::RectangleShape> borderRight;
 //vector for track segments
 std::vector<std::pair<float, float>> TrackVector;	//curvature, length
 
-void init(void);
+void init(float &);
 void update(sf::Color grassCol, sf::Color borderCol, float middle, float roadSize, float roadBorderLeft, float roadBorderRight, float roadBorderSize, int i);
+void updateCarSprite(float PlayerCurvature, float fCurvature, sf::Sprite &car, sf::Texture carTex);
 
 int main()
 {
 	sf::Clock clk;
 	sf::Time time;
-	sf::RenderWindow window(sf::VideoMode(WindowSize.x, WindowSize.y), "Test", sf::Style::Close);
+	sf::RenderWindow window(sf::VideoMode(WindowSize.x, WindowSize.y), "Pseudo_3D", sf::Style::Close);
 	window.setVerticalSyncEnabled(true);
 	float dt = 0.f;
 	float distance = 0.f, speed = 0.f;
-	float segmentDistance = 0.f;
+	float segmentDistance = 0.f, TotalDistance = 0.f;
 	int segmentIndex = 0;
 	float CurrentCurvature = 0.f, CurvatureDiff = 0.f, TrackCurvature = 0.f;
+	float PlayerCurvature = 0.f, fCurvature = 0.f;	//this is curvatrue of a single particle
 
-	init();
+	init(TotalDistance);
+
+	//car
+	sf::Sprite car;
+	sf::Texture carTexture;
+	carTexture.loadFromFile("Car_Sprite_Sheet.png");
+	car.setTexture(carTexture);
+	car.setTextureRect(sf::IntRect(200, 175, 40, 25));
+	car.setScale(4, 4);
+
 
 	while (window.isOpen())
 	{
@@ -54,6 +65,15 @@ int main()
 		if (speed < 0)speed = 0;
 		distance += speed * 100 * dt;
 
+		//player curvature handling
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+			PlayerCurvature -= 0.7*dt;
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+			PlayerCurvature += 0.7*dt;
+		if (fabs(PlayerCurvature - fCurvature) >= 0.7)
+			speed -= 4 * dt;
+		updateCarSprite(PlayerCurvature, fCurvature, car, carTexture);
+
 		//Curvature handling
 		segmentDistance = 0.f;
 		segmentIndex = 0;
@@ -63,9 +83,16 @@ int main()
 				segmentIndex++;
 			}
 		}
+		if (distance >= TotalDistance)	//in a loop
+			distance -= TotalDistance;
+
 		TrackCurvature = TrackVector[segmentIndex - 1].first;
 		CurvatureDiff = (TrackCurvature - CurrentCurvature)*dt*speed;
 		CurrentCurvature += CurvatureDiff;
+
+		fCurvature += CurrentCurvature * dt * speed;
+		float posX = PlayerCurvature - fCurvature;
+		car.setPosition(((WindowSize.x / 2) + (posX*WindowSize.x) / 2) - 70, WindowSize.y - 150);
 
 
 		for (int i = 0; i < nSize; i++) {
@@ -96,6 +123,8 @@ int main()
 			window.draw(*i);
 		for (auto i = borderRight.begin(); i != borderRight.end(); i++)
 			window.draw(*i);
+		window.draw(car);
+
 		window.display();
 
 	}
@@ -124,10 +153,35 @@ void update(sf::Color grassCol, sf::Color borderCol, float middle, float roadSiz
 	borderRight[i].setPosition(roadBorderRight - roadBorderSize, y);
 }
 
+void updateCarSprite(float PlayerCurvature, float fCurvature, sf::Sprite &car, sf::Texture carTex)
+{
+	float diff = PlayerCurvature - fCurvature;
+	if (diff < 0) {
+		if(fabs(diff) < 0.1)
+			car.setTextureRect(sf::IntRect(200, 175, 40, 25));
+		else if (fabs(diff) < 0.2)
+			car.setTextureRect(sf::IntRect(121, 175, 38, 25));
+		else if (fabs(diff) < 0.3)
+			car.setTextureRect(sf::IntRect(80, 175, 40, 25));
+		else
+			car.setTextureRect(sf::IntRect(35, 175, 45, 25));
+	}
+	else if (diff > 0) {
+		if (fabs(diff) < 0.1)
+			car.setTextureRect(sf::IntRect(200, 175, 40, 25));
+		else if (fabs(diff) < 0.2)
+			car.setTextureRect(sf::IntRect(280, 175, 38, 25));
+		else if (fabs(diff) < 0.3)
+			car.setTextureRect(sf::IntRect(320, 175, 40, 25));
+		else
+			car.setTextureRect(sf::IntRect(360, 175, 45, 25));
+	}
+}
 
-void init()
+void init(float &TotalDistance)
 {
 	sf::RectangleShape temp;
+	temp.setFillColor(sf::Color(128, 128, 128, 255));
 	for (int i = 0; i < nSize; i++) {
 		road.push_back(temp);
 		borderLeft.push_back(temp);
@@ -139,13 +193,19 @@ void init()
 		grass.push_back(temp);
 
 	//Track segemnts
-	TrackVector.push_back(std::make_pair(0.f, 100.f));
+	TrackVector.push_back(std::make_pair(0.f, 400.f));
+	TrackVector.push_back(std::make_pair(-0.2, 100.f));
+	TrackVector.push_back(std::make_pair(-0.5, 200.f));
+	TrackVector.push_back(std::make_pair(-0.8, 200.f));
+	TrackVector.push_back(std::make_pair(0.f, 300.f));
 	TrackVector.push_back(std::make_pair(-1.f, 100.f));
-	TrackVector.push_back(std::make_pair(0.f, 100.f));
+	TrackVector.push_back(std::make_pair(0.f, 300.f));
+	TrackVector.push_back(std::make_pair(0.2, 200.f));
+	TrackVector.push_back(std::make_pair(0.5, 200.f));
+	TrackVector.push_back(std::make_pair(0.2, 200.f));
 	TrackVector.push_back(std::make_pair(1.f, 100.f));
 	TrackVector.push_back(std::make_pair(0.f, 100.f));
-	TrackVector.push_back(std::make_pair(-0.2, 100.f));
-	TrackVector.push_back(std::make_pair(0.f, 50.f));
-	TrackVector.push_back(std::make_pair(0.2, 100.f));
-	TrackVector.push_back(std::make_pair(0.f, 100.f));
+
+	for (auto i : TrackVector)
+		TotalDistance += i.second;
 }
